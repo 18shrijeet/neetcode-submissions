d131b6f80547b8954d178b12eeb882778561c3f0
Shrijeet <40976061+18shrijeet@users.noreply.github.com>
2026-08-02 14:28:24 +0530

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int f= 0, s = 1;
+        for(int i=0;i<n;i++)
+        {
+            int temp = f+s;
+            f=s;
+            s= temp;
+        }
+        return s;
+    }
+};
